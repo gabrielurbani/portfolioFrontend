d5c52ae6a4b266f3397b4118850f0588c08283b0
“[Gabriel <[gabrielurbani@gmail.com]>
2022-05-09 20:38:13 -0300

    Header con componentes en el raiz

--- portfolioF_gUrbani/src/app/app.module.ts
+++ portfolioF_gUrbani/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SocialComponent } from './components/social/social.component';
+import { LoguinComponent } from './components/loguin/loguin.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    LogoAPComponent,
+    BannerComponent,
+    SocialComponent,
+    LoguinComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
